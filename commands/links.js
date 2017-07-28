bcee7420307c05c8d2725ffcfd8f9f768ffdaee5
Jellyfishe <chelle.wong@mail.utoronto.ca>
2017-07-28 00:21:29 -0400

    Added functionality for exams and shuttle bus

--- commands/links.js
+++ commands/links.js
@@ -2,10 +2,12 @@ exports.run = (client, msg, args) => {
     const shortcuts = {
         "UTMcutoff": "The CGPA requirement for both the May and August 2017 POSt admissions periods is 2.70 for the CSC major and all CSC specialists.",
         "SGcutoff": "The CGPA is",
-        "help": "PoST/Program Enrollment UTSG: http://www.artsci.utoronto.ca/current/program/enrolment-instructions/index_html \n" +
+        "help": "**FAQ**\n PoST/Program Enrollment UTSG: http://www.artsci.utoronto.ca/current/program/enrolment-instructions/index_html \n" +
           "PoST/Program Enrollment UTM: https://www.utm.utoronto.ca/registrar/office-registrar-publications/program-selection-guide-2017 \n" +
           "PoST/Program Enrollment UTSC: http://www.utsc.utoronto.ca/aacc/enrolling-subject-posts \n" +
-          "UTM Program Requirements: http://www.utm.utoronto.ca/programs-departments"
+          "UTM Program Requirements: http://www.utm.utoronto.ca/programs-departments \n" +
+          "UTM CGPA Calculator: https://student.utm.utoronto.ca/cgpa/ \n" +
+          "UTSC GPA Calculator: http://www.utsc.utoronto.ca/webapps/aacc-tools/GPA_calculator/index.cgi"
     };
     
     if(args.length == 0) return;
--- commands/ut.js
+++ commands/ut.js
@@ -1,67 +1,61 @@
-// I want this bot to function as: user searches a course code, can get a list to choose from (or if list returned is only one), then edit the message to whatever the user choose with more info.
 // start time of exams is in seconds
 
 //Does not do UTSC course codes.
-//If more than 3 results, DM the person to avoid spam.
+//Exam data has up to 2016 times.
 
 const rp = require("request-promise");
 
-const courseCodeRegex = /[A-Z]{3}\d{3}/;
-const shuttleRegex = /(shuttle)/;
+const courseCodeRegex = /^[A-Z]{3}\d{3}$/;
+const shuttleRegex = /^(shuttle)$/;
 const examRegex = /(exam)/;
 const year = "2017";
+//const period = "DEC17";
+const lim = 9;
+const DMTime = 3;
+const offset = 300;
 const numbers = "①②③④⑤⑥⑦⑧⑨";
-const pList = ["buildings", "textbooks", "food", "athletics", "exams", "transportation/parking", "transportation/shuttles", "cdf/labs", "cdf/printers"]; // eslint-disable-line
 
 exports.run = async(client, msg, args) => {
     const options = createSearchable(client.config, args);
 
     if (!options) return msg.channel.send("Cannot search that.");
 
-    const m = await msg.channel.send(":dvd: Searching now...");
-    msg.channel.startTyping();
+    let m = await msg.channel.send(":dvd: Searching now...");
+    m.channel.startTyping();
     
     try {
         const info = await rp(options);
-        if(info.length === 0) {
-            msg.channel.stopTyping();
+        if(info.length == 0) {
+            m.channel.stopTyping();
             return m.edit(":slight_frown: **||** Nothing came up.");
         }
-        
-        //const results = displayResults(info, args[0]);
         
         // Courses
         if(courseCodeRegex.test(args[0])){
             const results = displayCourses(info);
             
+            if(results.length > DMTime) m = await msg.member.createDM();
+            
             m.edit(":package: **||** Here are the result(s).");
-            results.forEach( (e, index) => {
-                msg.channel.send({embed: createEmbed(e, index)});
-            });
-        }else if(shuttleRegex.test(args[0])){ // Shuttle Times
-            msg.channel.send("Shuttle goes here.");
+            results.forEach((e, index) => m.channel.send({embed: createCourseEmbed(e, index)}));
+            
+        // Shuttle Times
+        }else if(shuttleRegex.test(args[0])){ 
+            m.channel.send({embed: displayShuttles(info)});
+            m.delete();
+        // Exam Times
         }else if(examRegex.test(args[0])){
-            msg.channel.send("Exams go here.");
+            if(args.length > 1 && courseCodeRegex.test(args[1].toUpperCase())) {
+                msg.channel.send({embed: displayExam(info)});
+                m.edit("This data was last updated for the `2016 year`.");
+            }
         }
-        msg.channel.stopTyping();
-        return;
-        
-        // Wait for response and react appropriately - currently only doable for courses. -TODO: expand this.
-        let response = await awaitReply(msg, "Please use a number to pick which one you would like to hear about.");
-        if (response && /\d/.test(response)) response = parseInt(response) - 1;
-        else return msg.channel.send("That is not a valid reply. Closing~");
-        
-        if(0 <= response && response < info.length) { //TODO: stuff
-            //msg.channel.send(courseDescription(info[response]));
-            
-            
-            
-        } else msg.channel.send("Invalid number.");
+        m.channel.stopTyping();
         
     } catch (e) {
-        if(client.user.typingIn(msg.channel)) msg.channel.stopTyping();
+        if(client.user.typingIn(m.channel)) m.channel.stopTyping();
         
-        msg.channel.send("There was an error, oh no!");
+        m.channel.send("There was an error, oh no! :dizzy_face:");
         console.log(e);
     }
 };
@@ -82,54 +76,151 @@ exports.help = {
 function createSearchable(config, args) {
     if(args.length == 0) return false;
     
-    let searchable = {
+    const searchable = {
         method: "GET",
         headers: {
             "Authorization": config.cobalt_key
         },
         qs: {
-            limit: 9
+            limit: lim
         },
         json:true
     };
     
-    let searchMethod;
     if(courseCodeRegex.test(args[0])){
-        searchable.uri = config.cobalt + 'courses/filter';
-        if(args.length > 1){
-            searchable.qs.q = filterQueryFormat(args[0], args[1])
-        }else {
-            searchable.qs.q = filterQueryFormat(args[0]);
-        }
+        searchable.uri = `${config.cobalt}courses/filter`;
+        if(args.length > 1) {
+            args[0] = args[0].toUpperCase();
+            args[1] = args[1].toUpperCase();
+            
+            if (['UTSG', 'UTM'].includes(args[1])) searchable.qs.q = `code:"${args[0]}" AND campus:"${args[1]}" AND term:"${year}"`;
+            else return false;
+            
+        } else searchable.qs.q = `code:"${args[0].toUpperCase()}" AND term:"${year}"`;
+        
+    }else if (examRegex.test(args[0])) {
+        searchable.uri = `${config.cobalt}exams/filter`;
+        //searchable.qs.q = `code:"${args[1].toUpperCase()}" AND period:"${period}"`;
+        searchable.qs.q = `code:"${args[1].toUpperCase()}"`;
+        
+    }else if (shuttleRegex.test(args[0])) {
+        const now = new Date().toISOString().substr(0, 10);
+        searchable.uri = `${config.cobalt}transportation/shuttles/${now}`;     
+        delete searchable.qs;
     }else return false;
 
     return searchable;
 }
 
-/** 
-  * Function that grabs a reply from the user who started this command
-  *
-  * @param {Message}    msg        - Message Object to know which channel to listen for a reply from
-  * @param {string}     question   - String prompt to display to the user
-  * @param {int}        limit      - Integer representing how many milliseconds to wait for a reply
+/**
+  * Function that concatenates courses with the same code into one object in the list.
+  * @param {list} info - 
   */
-async function awaitReply(msg, question, limit = 60000) {
-    const filter = m => m.author.id = msg.author.id;
-    await msg.channel.send(question);
-    await wait(1000); // eslint-disable-line
-    try {
-        const collected = await msg.channel.awaitMessages(filter, {
-            max: 1,
-            time: limit,
-            errors: ["time"]
-        });
-        return collected.first().content;
-    } catch (e) {
-        msg.channel.send("Time limit reached.");
-        return false;
-    }
+function displayCourses(info){
+    const courses = [info[0]];
+    courses[0].code = courses[0].code.slice(0,9);
+    
+    info.slice(1).forEach(course => {
+        const check = courses.filter(e => e.code.includes(course.code.slice(0, 8)));
+        if(check.length > 0){
+            check[0].term += `\n${course.term}`;
+        }else {
+            courses.push(course);
+            courses[courses.length - 1].code = course.code.slice(0,9);
+        }
+    });
+    
+    return courses;
 }
 
+function displayShuttles(info){
+    let utmTimes='', sgTimes='';
+    info.routes[0].stops[0].times.forEach((e, index) => {
+        utmTimes += secondsToTimeFormat(e.time).toString().substring(-8) + ` ${e.rush_hour ? '`**`' : ''}\n`;
+        sgTimes += secondsToTimeFormat(info.routes[0].stops[1].times[index].time) + ` ${info.routes[0].stops[1].times[index].rush_hour ? '`**`' : ''}\n`;
+    });
+    return {
+        "title": `Shuttle Times for ${new Date(info.date).toDateString()} :oncoming_bus:`,
+        "color": 16777215,
+        "description": "`**` indicates rush hour.\n The regular one-way ticket fare is $6.00.",
+        "timestamp": new Date(),
+        "footer": {
+            "text": "Brought to you by the Cobalt API"
+        },
+        "fields": [
+            {
+                "name": `IB Layby :busstop:`,
+                "value": utmTimes,
+                "inline": true
+            },
+            {
+                "name": `Hart House :busstop:`,
+                "value": sgTimes,
+                "inline": true
+            }
+            
+        ]
+    };
+}
+
+function secondsToTimeFormat(time, showTimeZone=false){
+    const options = { hour: 'numeric',minute:'numeric', hour12: true};
+    if(showTimeZone) options.timeZoneName = 'short';
+    
+    return new Date(time * 1000 + offset*60000).toLocaleTimeString("en-ca", options);
+}
+
+//TODO: Fix this so it doesn't just grab the first result only.
+function displayExam(info){
+    /*const options = { hour: 'numeric',minute:'numeric', hour12: true};
+    const startTime = new Date(info[0].start_time*1000 + offset*60000).toLocaleTimeString("en-ca", options);
+    options.timeZoneName = 'short';
+    const endTime = new Date(info[0].end_time*1000 + offset*60000).toLocaleTimeString("en-ca", options);*/
+    const startTime = secondsToTimeFormat(info[0].start_time);
+    const endTime = secondsToTimeFormat(info[0].end_time, true);
+    
+    return {
+        "title":  info[0].course_code + ' Exam Time :clock:',
+        "color": 16777215,
+        "timestamp": new Date(),
+        "footer": {
+            "text": "Brought to you by the Cobalt API"
+        },
+        "fields": [
+            {
+                "name": "Date",
+                "value": new Date(info[0].date).toDateString(),
+                "inline": true
+            },
+            {
+                "name": "Time",
+                "value": `${startTime.toString().substring(-8)} - ${endTime.toString().substring(-8)}`,
+                "inline": true
+            },
+            {
+                "name": "Exam Locations",
+                "value": displaySections(info[0].sections)
+            }
+            
+        ]
+    };
+}
+
+function displaySections(sections){
+    let thing = "";
+    let lectureCode = "";
+    sections.forEach(section => {
+        if(lectureCode !== section.lecture_code){
+            thing += `**${section.lecture_code}** \n`;
+            lectureCode = section.lecture_code;
+        }
+        thing += `${section.exam_section ? section.exam_section : section.location}    ${section.exam_section ? section.location.trim : section.exam_section} \n`;
+    });
+    return thing;
+    
+}
+            
+    
 /**
   * Function that formats text to display to the user information obtained from the API
   * @param {} info
@@ -153,27 +244,18 @@ function displayResults(info, query){
     return results;
 }
 
-
-function filterQueryFormat(code, campus=''){
-    code = code.toUpperCase();
-    campus = campus.toUpperCase();
-    
-    if(campus) return 'code:"' + code + '" AND campus:"' + campus + '" AND term:"' + year + '"';
-    else return 'code:"' + code + '" AND term:"' + year + '"';
-}
-
 /**
   * Creates the option for embed.
   *
   */
-function createEmbed(msg, index){
+function createCourseEmbed(msg, index){
     return {
-        "title": numbers[index] + ' ' + msg.code,
+        "title": `${numbers[index]} ${msg.code.slice(0, -1)}`,
         "description": msg.description,
         "color": 16777215,
         "timestamp": new Date(),
         "footer": {
-            "text": "Brought to you by ROSI"
+            "text": "Brought to you by the Cobalt API"
         },
         "fields": [
             {
@@ -190,24 +272,27 @@ function createEmbed(msg, index){
         ]
     };
 }
-/**
-  * Function that concatenates courses with the same code into one object in the list.
-  * @param {list} info - 
+
+/** 
+  * Function that grabs a reply from the user who started this command
+  *
+  * @param {Message}    msg        - Message Object to know which channel to listen for a reply from
+  * @param {string}     question   - String prompt to display to the user
+  * @param {int}        limit      - Integer representing how many milliseconds to wait for a reply
   */
-function displayCourses(info){
-    // Parse the info
-    let courses = [info[0]];
-    courses[0].code = courses[0].code.slice(0,9);
-    
-    info.slice(1).forEach(course => {
-        let check = courses.filter(e => e.code.includes(course.code.slice(0, 8)));
-        if(check.length > 0){
-            check[0].term += '\n' + course.term;
-        } else {
-            courses.push(course);
-            courses[courses.length - 1].code = course.code.slice(0,9);
-        }
-    });
-    
-    return courses;
+async function awaitReply(msg, question, limit = 60000) {
+    const filter = m => m.author.id === msg.author.id;
+    await msg.channel.send(question);
+    await wait(1000); // eslint-disable-line
+    try {
+        const collected = await msg.channel.awaitMessages(filter, {
+            max: 1,
+            time: limit,
+            errors: ["time"]
+        });
+        return collected.first().content;
+    } catch (e) {
+        msg.channel.send("Time limit reached.");
+        return false;
+    }
 }
